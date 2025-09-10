b34362c42738064afbbebf9f281a91206081c07d
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-10 16:09:00 +0200

    [add] feat import BrowserRouter Routes Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import AboutUs from "./pages/AboutUs"
+import Products from "./pages/Products"
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
